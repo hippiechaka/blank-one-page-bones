0207f9139b1e88e692f6f90c80eedce765a4e18f
hippieperro <hippiechaka@MacBook-Pro-de-Armando.local>
2017-04-22 12:05:58 -0500

    Inicial
    Subida Inicial

---
+++ sections_es/content.php
@@ -0,0 +1,87 @@
+<div class="clearfix" style="height:90px;"></div><!-- ALTO DE NAVBAR-->
+<!-- navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="" class="navbar-brand">
+					<img src="http://placehold.it/180x65" alt="">
+				</a>
+			</div>
+			<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#nav-res" aria-expanded="true">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+
+			<ul class="nav navbar-nav list-inline">
+
+				<li><a href="">Page 1</a></li>
+				<li><a href="">Page 2</a></li>
+				<li><a href="">Page 3</a></li>
+				<li><a href="">Page 4</a></li>
+				<li><a href="">Page 5</a></li>
+
+				<li class="divider"></li>
+
+				<li class="idioma">
+					<a href="" class="flagIdioma" id="idioma1">
+						<img src="http://placehold.it/30x30" alt="">
+					</a>
+				</li>
+				<li class="idioma">
+					<a href="" class="flagIdioma" id="idioma2">
+						<img src="http://placehold.it/30x30" alt="">
+					</a>
+				</li>
+
+				<!-- REDES SOCIALES -->
+
+				<li class="redesBtn">
+					<a href="" class="redesIcon" id="facebook">
+						<i class="fa fa-facebook" aria-hidden="true"></i>
+					</a>
+				</li>
+				<li class="redesBtn">
+					<a href="" class="redesIcon" id="instagram">
+						<i class="fa fa-instagram" aria-hidden="true"></i>
+					</a>
+				</li>
+				<li class="redesBtn">
+					<a href="" class="redesIcon" id="youtube">
+						<i class="fa fa-youtube" aria-hidden="true"></i>
+					</a>
+				</li>
+
+			</ul>
+		</div>
+	</div>
+</nav>
+<!-- BLOCK -->
+<header class="block">
+	<section id="mainslideshow" class="container-fluid owl-corousel">
+		<div>
+	        <img src="http://placehold.it/1920x900?text=TITULO-1" alt="desc" class="visible-lg-block">
+	        <img src="http://placehold.it/1200x640?text=TITULO-1" alt="desc" class="hidden-lg"> 	
+	    </div>
+	    <div>
+	        <img src="http://placehold.it/1920x900?text=TITULO-2" alt="desc" class="visible-lg-block">
+	        <img src="http://placehold.it/1200x640?text=TITULO-2" alt="desc" class="hidden-lg"> 	
+	    </div>
+	    <div>
+	        <img src="http://placehold.it/1920x900?text=TITULO-3" alt="desc" class="visible-lg-block">
+	        <img src="http://placehold.it/1200x640?text=TITULO-3" alt="desc" class="hidden-lg"> 	
+	    </div>
+	    <div>
+	        <img src="http://placehold.it/1920x900?text=TITULO-4" alt="desc" class="visible-lg-block">
+	        <img src="http://placehold.it/1200x640?text=TITULO-4" alt="desc" class="hidden-lg"> 	
+	    </div>
+	    <div>
+	        <img src="http://placehold.it/1920x900?text=TITULO-5" alt="desc" class="visible-lg-block">
+	        <img src="http://placehold.it/1200x640?text=TITULO-5" alt="desc" class="hidden-lg"> 	
+	    </div>
+	</section>
+</header>
+<!-- mainslideshow -->
+
+
